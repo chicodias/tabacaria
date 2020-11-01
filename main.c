58cba4106ae8cb42dd2f8ed2e3ad35652fc72d25
chicodias <chicodias@outlook.com>
2020-11-01 20:42:20 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/* 
+    Arquivo: main.c
+    Autor: Matheus Pires
+
+    função que faz o chamado as rotinas de execução da tabacaria
+ */
+#include "loja.h"
+
+
+int main()
+{
+    // chama o menu principal da aplicação
+    menu();
+
+    return 0;
+}
